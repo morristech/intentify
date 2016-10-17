d84ef8b6de79c294de88ee2b96a182dd6a7ace01
Willem Meints <willem@mein-design.nl>
2016-10-17 08:44:42 +0200

    Fix typo in buildscript

--- travis.sh
+++ travis.sh
@@ -2,5 +2,5 @@
 
 gradle sonarqube -Dsonar.github.oauth=$GITHUB_TOKEN \
     -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-    -Dsonar.github.pullRequest=$TRAVI_SPULL_REQUEST \
+    -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
     -Dsonar.analysis.mode=preview

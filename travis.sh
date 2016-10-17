5e63e7c301243a0c1b06e115380083b67716471b
Willem Meints <willem@mein-design.nl>
2016-10-17 08:39:03 +0200

    Fix property spec

--- travis.sh
+++ travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gradle sonarqube -Psonar.github.oauth=$GITHUB_TOKEN \
-    -Psonar.github.repository=$TRAVIS_REPO_SLUG \
-    -Psonar.github.pullRequest=$TRAVI_SPULL_REQUEST \
-    -Psonar.analysis.mode=preview
+gradle sonarqube -Dsonar.github.oauth=$GITHUB_TOKEN \
+    -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+    -Dsonar.github.pullRequest=$TRAVI_SPULL_REQUEST \
+    -Dsonar.analysis.mode=preview

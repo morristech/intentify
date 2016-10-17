dd6041e4b0bd4d6d6eaf169afed5c8aac6ab343b
Willem Meints <willem@mein-design.nl>
2016-10-17 08:52:30 +0200

    Enable master builds

--- travis.sh
+++ travis.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-gradle sonarqube -Dsonar.github.oauth=$GITHUB_TOKEN \
-    -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-    -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-    -Dsonar.analysis.mode=preview
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo 'Master branch: trigger QA'
+    gradle sonarqube
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
+    echo 'Pull request: trigger QA and preview analysis'
+
+    gradle sonarqube -Dsonar.github.oauth=$GITHUB_TOKEN \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.analysis.mode=preview
+fi

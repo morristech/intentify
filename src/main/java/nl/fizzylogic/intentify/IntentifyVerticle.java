aa87a1de1efff4a48ea8148eec58e1d1a167724f
Willem Meints <willem@mein-design.nl>
2016-10-17 10:03:36 +0200

    Add implementation of cassandra storage provider

--- src/main/java/nl/fizzylogic/intentify/IntentifyVerticle.java
+++ src/main/java/nl/fizzylogic/intentify/IntentifyVerticle.java
@@ -13,12 +13,13 @@ import nl.fizzylogic.intentify.training.TrainingVerticle;
 public class IntentifyVerticle extends AbstractVerticle {
     /**
      * Starts the application
-     * @param startFuture   Future to complete when the startup procedure is finished
-     * @throws Exception    Gets thrown when startup fails
+     *
+     * @param startFuture Future to complete when the startup procedure is finished
+     * @throws Exception Gets thrown when startup fails
      */
     @Override
     public void start(Future<Void> startFuture) throws Exception {
-        TrainingSampleService trainingSampleService = new TrainingSampleServiceImpl();
+        TrainingSampleService trainingSampleService = new TrainingSampleServiceImpl("intentify", "127.0.0.1");
 
         vertx.deployVerticle(new RestServiceVerticle());
         vertx.deployVerticle(new TrainingVerticle(trainingSampleService));
--- src/main/java/nl/fizzylogic/intentify/training/TrainingSampleServiceImpl.java
+++ src/main/java/nl/fizzylogic/intentify/training/TrainingSampleServiceImpl.java
@@ -1,14 +1,61 @@
 package nl.fizzylogic.intentify.training;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.PoolingOptions;
+import com.datastax.driver.core.querybuilder.Insert;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
 import nl.fizzylogic.intentify.entities.TrainingSample;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Implementation of the {@link TrainingSampleService}
  */
 public class TrainingSampleServiceImpl implements TrainingSampleService {
 
+    private final Cluster cluster;
+    private final String databaseName;
+
+    /**
+     * Initializes a new instance of {@link TrainingSampleServiceImpl}
+     *
+     * @param databaseName    Name of the keyspace to connect to
+     * @param serverAddresses List of server addresses for the cassandra cluster
+     */
+    public TrainingSampleServiceImpl(String databaseName, String... serverAddresses) {
+        this.databaseName = databaseName;
+        PoolingOptions poolingOptions = new PoolingOptions();
+
+        cluster = Cluster.builder()
+                .addContactPoints(serverAddresses)
+                .withPoolingOptions(poolingOptions)
+                .build();
+    }
+
+    /**
+     * Stores a sample in cassandra the data store
+     *
+     * @param sample Sample to store
+     */
     @Override
     public void storeSample(TrainingSample sample) {
+        Insert statement = QueryBuilder
+                .insertInto(databaseName, "samples")
+                .values(
+                        listOf("intent", "text"),
+                        listOf(sample.getIntent(), sample.getText())
+                );
 
+        cluster.connect().execute(statement);
+    }
+
+    @SafeVarargs
+    private static <T> List<T> listOf(T... items) {
+        List<T> results = new ArrayList<>(items.length);
+        Collections.addAll(results, items);
+
+        return results;
     }
 }
